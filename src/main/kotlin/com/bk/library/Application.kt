df326174991b63fa293213ffe3e73b29f93c30f3
kothawaleb <kothawaleb@gmail.com>
2024-08-21 11:11:40 +0530

    demo branch

--- src/main/kotlin/com/bk/library/Application.kt
+++ src/main/kotlin/com/bk/library/Application.kt
@@ -1,5 +1,18 @@
 package com.bk.library
 
+import io.ktor.server.engine.*
+import io.ktor.server.http.content.*
+import io.ktor.server.netty.*
+import io.ktor.server.routing.*
+
 fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
+    EngineMain.main(args)
+//    embeddedServer(Netty, port = 3000, host = " 0.0.0.0") {
+//        routing {
+//            singlePageApplication{
+//                react()
+//            }
+//        }
+//    }
+
 }
---
+++ src/main/kotlin/com/bk/library/client/ReactClient.kt
@@ -0,0 +1,16 @@
+package com.bk.library.client
+
+import io.ktor.server.application.*
+import io.ktor.server.http.content.*
+import io.ktor.server.routing.*
+
+fun Application.reactClientApp() {
+    routing {
+        singlePageApplication {
+            useResources = true
+            filesPath = "library-web-app"
+            defaultPage = "index.html"
+//            ignoreFiles { it.endsWith(".txt") }
+        }
+    }
+}
